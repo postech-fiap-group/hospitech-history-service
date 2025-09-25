ca75fbc3f02e60aaeb22b326c74215d4dd025e1d
armanoalves <armanobarrosalvesh@gmail.com>
2025-09-25 17:09:40 -0300

    first commit

---
+++ src/main/java/dev/call/hospitech_history_service/HospitechHistoryServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.call.hospitech_history_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitechHistoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitechHistoryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/call/hospitech_history_service/HospitechHistoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.call.hospitech_history_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitechHistoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
